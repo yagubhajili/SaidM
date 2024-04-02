4b0004cb5f129625e58f912e3b26d9545e831e4e
yagubhajili <yaqubhacili6@gmail.com>
2024-04-03 03:39:08 +0400

    alqoritmler hazir

--- AlqoritSeidM/app.js
+++ AlqoritSeidM/app.js
@@ -1,33 +1,69 @@
 
 
-const alma = 2;
-const portagal = 3;
-const banan = 5;
+// const alma = 2;
+// const portagal = 3;
+// const banan = 5;
 
 
 
-let fruit = prompt("meyve adi daxil edin")
-let kilogram = +prompt("meyvenin kilosunu daxil edin")
+// let fruit = prompt("meyve adi daxil edin")
+// let kilogram = +prompt("meyvenin kilosunu daxil edin")
 
 
-if (!isNaN(kilogram)) {
-    switch (fruit) {
-        case "alma":
-            alert(`${kilogram * alma} AZN`)
-            break;
-        case "portagal":
-            alert(`${kilogram * portagal} AZN`)
+// if (!isNaN(kilogram)) {
+//     switch (fruit) {
+//         case "alma":
+//             alert(`${kilogram * alma} AZN`)
+//             break;
+//         case "portagal":
+//             alert(`${kilogram * portagal} AZN`)
 
-            break;
-        case "banan":
-            alert(`${kilogram * banan} AZN`)
+//             break;
+//         case "banan":
+//             alert(`${kilogram * banan} AZN`)
 
-            break;
+//             break;
 
-        default:
-            alert(`${fruit} duzgun meyve daxil edin`)
-            break;
-    }
-} else {
-    alert("kilogrami eded olaraq daxil edin")
-}
+//         default:
+//             alert(`${fruit} duzgun meyve daxil edin`)
+//             break;
+//     }
+// } else {
+//     alert("kilogrami eded olaraq daxil edin")
+// }
+
+
+
+// let array = [0, 1]
+
+
+// for (let i = 0; i <= 10; i++) {
+//     let l = array.length
+//     let k = array[l - 1] + array[l - 2]
+
+//     array.push(k)
+//     console.log(array[l])
+
+// }
+
+// for (let i = 1; i <= 10; i++) {
+
+//     for (let j = 1; j <= 10; j++) {
+
+//         console.log(`${i}x${j}=${i * j}`)
+//     }
+//     console.log("----------------------")
+
+// }
+
+
+
+// let array = [0, 1, 45, 10, 1323, 323, 54, 1, 36, 6]
+// let max = array[0]
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] > max) {
+//         max = array[i]
+//     }
+// }
+// console.log(max)
