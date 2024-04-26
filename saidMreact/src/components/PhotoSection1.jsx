222596e18cdd970ff5ffecd86590e965f5103df3
yagubhajili <yaqubhacili6@gmail.com>
2024-04-26 11:38:05 +0400

    react bootstrap landing page ready

---
+++ saidMreact/src/components/PhotoSection1.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './photosection.css'
+
+const PhotoSection1 = () => {
+    return (
+        <div className='photoSection1'>
+            <div className='TextHolder container'>
+                <h2>
+                    Fully Responsive Design
+                </h2>
+                <p>When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+
+            </div>
+            <div className='imageHolder1'>
+
+            </div>
+        </div>
+    )
+}
+
+export default PhotoSection1
---
+++ saidMreact/src/components/PhotoSection2.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const PhotoSection2 = () => {
+    return (
+        <div className='photoSection2'>
+            <div className='imageHolder2'>
+
+            </div>
+            <div className='TextHolder container'>
+                <h2>
+                    Updated For Bootstrap 5
+                </h2>
+                <p>When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default PhotoSection2
